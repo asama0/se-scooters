f9b0a6d71f712e9bf77f7233c02f4538610ca78a
fy19sbsh <fy19sbsh@leeds.ac.uk>
2022-02-06 20:09:42 +0000

    add some commend

--- components/sendemail.py
+++ components/sendemail.py
@@ -5,14 +5,14 @@ from email.message import EmailMessage
 
 massage = EmailMessage()
 massage['Subject'] = 'test it'
-massage['From'] = 'salimbader734@gmail.com'
-massage['To'] = 'salimbader18@gmail.com'
+massage['From'] = 'salimbader734@gmail.com' # user email address
+massage['To'] = 'salimbader18@gmail.com' # to who you want send email 
 
 massage.set_content('hi our nice group')
 
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    smtp.login("salimbader734@gmail.com", "123456SS")
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp: # 465 is the gmail port number
+    smtp.login("salimbader734@gmail.com", "123456SS")# user email address and password
     smtp.send_message(massage)
 
     
