d2bc4072cb4b66858ee7500ccb692d01e5f42889
fy19amaa <fy19amaa@leeds.ac.uk.com>
2022-03-29 14:56:08 +0100

    Added parkings and and scooters csv

---
+++ db_manager/db_parkings.py
@@ -0,0 +1,20 @@
+from app import db
+from app.models import Parking
+import csv
+
+with open('csv/parkings.csv', 'w', newline='') as file:
+    csv_reader = csv.DictReader(file, delimiter=',')
+
+    for line_count, row in enumerate(csv_reader):
+        if line_count == 0:
+            continue
+
+        new_parking = Parking(
+            name=row['name'],
+            latitude=float(row['latitude']),
+            longitude=float(row['longitude'])
+        )
+
+        db.session.add(new_parking)
+
+    db.session.commit()
--- db_manager/db_users.py
+++ db_manager/db_users.py
@@ -2,6 +2,7 @@ from app import db, bcrypt
 from app.models import User
 import stripe
 import csv
+from datetime import datetime
 
 with open('csv/users.csv', 'w', newline='') as file:
     csv_reader = csv.DictReader(file, delimiter=',')
@@ -12,11 +13,12 @@ with open('csv/users.csv', 'w', newline='') as file:
 
         hashedPassword = bcrypt.generate_password_hash(row['password']).decode('utf-8')
         new_stripe_id = stripe.Customer.create()['id']
-        new_user = User(name=row['name'], email=row['email'],
-                    password=hashedPassword, birth_date=row['birth_date'],
-                    phone=row['phone'], stripe_id=new_stripe_id,
-                    privilege=row['privilege']
-                )
+        new_user = User(
+            name=row['name'], email=row['email'], password=hashedPassword,
+            birth_date=datetime.strptime(row['birth_date'], '%d/%m/%Y'),
+            phone=int(row['phone']), stripe_id=new_stripe_id,
+            privilege=int(row['privilege'])
+        )
 
         db.session.add(new_user)
 
