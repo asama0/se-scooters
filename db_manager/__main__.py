4cebad9782121b06922389f0dec82b16dc9b2158
fy19amaa <fy19amaa@leeds.ac.uk.com>
2022-03-29 16:36:49 +0100

    Edited stripe functions. And added to the database via db_prices.py

--- db_manager/__main__.py
+++ db_manager/__main__.py
@@ -7,5 +7,6 @@ print('Database has been created!')
 from db_manager import db_users
 from db_manager import db_parkings
 from db_manager import db_scooters
+from db_manager import db_prices
 
 print('Database entries have been added successfully!')
