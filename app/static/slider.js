83f2d3f207da991300125999fe33b0b5512b32f3
Alyaqdahn <Fy19ayyn@leeds.ac.uk>
2022-03-31 18:49:41 +0100

    Change dropdown menu to collapse

--- app/static/slider.js
+++ app/static/slider.js
@@ -2,10 +2,12 @@ jQuery(document).ready(function($) {
   var alterClass = function() {
     var ww = document.body.clientWidth;
     if (ww < 575) {
-      $('#list').addClass('dropdown-menu');
+      $('#list').addClass('collapse');
+      $('#list').addClass('card card-body');
       $('#list').removeClass('nav');
     } else if (ww >= 575) {
-      $('#list').removeClass('dropdown-menu');
+      $('#list').removeClass('collapse');
+      $('#list').removeClass('card card-body');
       $('#list').addClass('nav');
     };
   };
